fab8264301806837756218a5028374e4202ad9fe
Your Name <you@example.com>
2022-11-26 16:31:03 +1300

    Created form.py under accounts folder

--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.urls import reverse_lazy
 from django.views import generic
-
+from .forms import CreateUserForm
 
 from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CreateUserForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"

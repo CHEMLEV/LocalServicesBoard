a68be7f1b8fef71f3b6cb8c0a6e5046c465b6d0e
CHEMLEV <88120172+CHEMLEV@users.noreply.github.com>
2022-11-27 08:32:02 +1300

    Dead code removed from files in 'accounts' folder

--- accounts/views.py
+++ accounts/views.py
@@ -3,9 +3,6 @@ from django.urls import reverse_lazy
 from django.views import generic
 from .forms import CreateUserForm
 
-from django.conf import settings
-User = settings.AUTH_USER_MODEL
-
 
 class SignUpView(generic.CreateView):
     form_class = CreateUserForm

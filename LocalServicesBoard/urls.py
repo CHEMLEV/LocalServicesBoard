eeb02e8b9f014b6c6dc2bd056e58ddaec8b24b76
Mei-Hsiu <mche213@mywhitecliffe.com>
2022-12-06 08:31:26 +1300

    Modify add review function and add review edit and delete function

--- LocalServicesBoard/urls.py
+++ LocalServicesBoard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MainPageView, ClassifiedsListView, ClassifiedDetailsView, AboutPageView, ClassifiedUpdateView, ClassifiedDeleteView, ClassifiedCreateView
+from .views import MainPageView, ClassifiedsListView, ClassifiedDetailsView, AboutPageView, ClassifiedUpdateView, ClassifiedDeleteView, ClassifiedCreateView, ReviewUpdateView, ReviewDeleteView, ReviewCreateView
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -14,6 +14,9 @@ urlpatterns = [
     path("classified/<int:pk>/edit", ClassifiedUpdateView.as_view(), name = "classified_edit" ),
     path("classified/<int:pk>/delete", ClassifiedDeleteView.as_view(), name = "classified_delete" ),
     path("classified/new/", ClassifiedCreateView.as_view(), name = "classified_new"),
+    path("review/<int:pk>/edit", ReviewUpdateView.as_view(), name = "review_edit" ),
+    path("review/<int:pk>/delete", ReviewDeleteView.as_view(), name = "review_delete" ),
+    path("review/<int:pk>/new/", ReviewCreateView.as_view(), name = "review_new"),
     
 ]
 
